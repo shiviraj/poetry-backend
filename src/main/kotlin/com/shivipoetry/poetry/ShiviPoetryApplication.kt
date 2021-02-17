a07735aa59bc209980ee4a690ed984beb7e7cdae
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-02-17 08:44:24 +0530

    first commit

---
+++ src/main/kotlin/com/shivipoetry/poetry/ShiviPoetryApplication.kt
@@ -0,0 +1,11 @@
+package com.shivipoetry.poetry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShiviPoetryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShiviPoetryApplication>(*args)
+}
---
+++ src/test/kotlin/com/shivipoetry/poetry/ShiviPoetryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shivipoetry.poetry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShiviPoetryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
